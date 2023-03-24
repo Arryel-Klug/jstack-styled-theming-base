2d28629f79c1db02ba7d6f92653d3fe57a9c6806
Arryel-Kauhu <arryel.klug@gmail.com>
2023-03-24 10:12:12 -0300

    Teste chave ssh

--- src/components/PostsList/Post/index.js
+++ src/components/PostsList/Post/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Post({ title, description }) {
-  return (
-    <Container>
-      <h2>{title}</h2>
-      <small>{description}</small>
-    </Container>
-  );
+class PostsList extends ReactComponent {
+  render(){
+    
+  }
 }
+
+
+
+// export default function Post({ title, description }) {
+//   return (
+//     <Container>
+//       <h2>{title}</h2>
+//       <small>{description}</small>
+//     </Container>
+//   );
+// }
